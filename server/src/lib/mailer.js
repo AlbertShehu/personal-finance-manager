bee68157596f09b1036da30dde156d62119ed8f3
Shehu <shehus722@gmail.com>
2025-09-28 15:23:21 +0200

    fix: update email config to use port 465, add prisma generate to build process

--- server/src/lib/mailer.js
+++ server/src/lib/mailer.js
@@ -15,7 +15,9 @@ function getMailer() {
   }
 
   transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true për 465, false për 587
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
